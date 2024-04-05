062dd0e55c921dc88f94b9dcf868381ffbe1e284
AndreyBV5 <andrey0130player@gmail.com>
2024-04-05 08:56:45 -0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,72 +5,47 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Contador con GetX',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final CountController _controller = Get.put(CountController());
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Contador con GetX'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            GetBuilder<CountController>(
-              builder: (_) => Text(
-                'Contador: ${_.count}',
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    _controller.increment();
-                  },
-                  child: Icon(Icons.add),
-                ),
-                SizedBox(width: 20.0),
-                ElevatedButton(
-                  onPressed: () {
-                    _controller.decrement();
-                  },
-                  child: Icon(Icons.remove),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CountController extends GetxController {
+class CounterController extends GetxController {
   var count = 0.obs;
 
   void increment() {
-    count++;
+    count++; // Incrementa el contador
   }
+}
 
-  void decrement() {
-    count--;
+class MyApp extends StatelessWidget {
+  final CounterController _controller = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Contador con GetX'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Obx(() => Text(
+                'Contador: ${_controller.count}',
+                style: TextStyle(fontSize: 24),
+              )),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  _controller.increment(); // Incrementa el contador
+                },
+                child: Text('Incrementar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
